698ef90e1d42c0ab7003df55bb4b16bc8ef5d97f
seftianandy <seftianandy45@gmail.com>
2025-03-18 18:53:46 +0700

    Update footer panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
             ->topNavigation()
+            ->renderHook('panels::body.end', fn () => view('filament.components.admin-footer'))
             ->pages([])
             // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
---
+++ resources/views/filament/components/admin-footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-gray-200 text-center py-4 text-sm text-gray-600">
+    Copyright &copy; {{ now()->year }} MEGABOY - Arsip Application By TIM IT SMKN 3 Boyolangu. All Rights Reserved.
+</footer>
