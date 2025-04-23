a30cac841bbb0ecb55a6abde36305166cd14ec2d
seftianandy <seftianandy45@gmail.com>
2025-04-23 11:39:03 +0700

    Update admin-footer.blade.php

--- resources/views/filament/components/admin-footer.blade.php
+++ resources/views/filament/components/admin-footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="bg-gray-200 text-center py-4 text-sm text-gray-600">
     <strong>Copyright &copy; {{ now()->year }} MEGABOY - Arsip Application By TIM IT SMKN 3 Boyolangu</strong>. All Rights Reserved.
-    <span class="text-right"><strong>Version App</strong> 1.5.2 in use.</span>
+    <span class="text-right"><strong>Version App</strong> 1.5.3 in use.</span>
 </footer>
