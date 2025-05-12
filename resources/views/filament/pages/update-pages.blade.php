476baccf6caf81f082456a91ff2bf539ce5bd078
seftianandy <seftianandy45@gmail.com>
2025-05-12 08:20:26 +0700

    Update halaman update aplikasi
    Update versi aplikasi ke 1.5.1

--- resources/views/filament/pages/update-pages.blade.php
+++ resources/views/filament/pages/update-pages.blade.php
@@ -19,17 +19,26 @@
 
     <div class="p-6 bg-white shadow rounded-xl dark:divide-white/10 dark:bg-gray-900 dark:ring-white/10">
         <h2 class="text-lg font-bold text-gray-800 mb-4">Riwayat Update</h2>
-
         <div class="overflow-x-auto">
-            <ul>
+            <ul class="list-none space-y-4">
                 <li>
-                    <strong># Rilis Update Aplikasi 1.5.0</strong> <small>(23 April 2025)</small>
+                    <div class="font-bold text-gray-700"># Rilis Update Aplikasi 1.5.1</div>
+                    <small class="text-gray-500">(12 Mei 2025)</small>
+                    <ul class="list-disc list-inside pl-5 mt-1 space-y-2 text-gray-800">
+                        <li>
+                            Fitur contoh surat keluar untuk pengajuan surat keluar. <br>
+                            User dapat mengunggah dan mengunduh contoh surat keluar sesuai dengan jenis surat yang dibutuhkan.
+                        </li>
+                    </ul>
                 </li>
+
                 <li>
-                    - Fitur generate dan download QRCode Barcode untuk TTE pada surat keluar.
-                </li>
-                <li>
-                    - Menu update aplikasi untuk melakukan update sistem aplikasi secara manual.
+                    <div class="font-bold text-gray-700"># Rilis Update Aplikasi 1.5.0</div>
+                    <small class="text-gray-500">(23 April 2025)</small>
+                    <ul class="list-disc list-inside pl-5 mt-1 space-y-2 text-gray-800">
+                        <li>Fitur generate dan download QRCode Barcode untuk TTE pada surat keluar.</li>
+                        <li>Menu update aplikasi untuk melakukan update sistem aplikasi secara manual.</li>
+                    </ul>
                 </li>
             </ul>
         </div>
